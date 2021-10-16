c062847a844fa9a800f1cbcd88dbd716d0a98414
Irina Novakovich <irinaredhood@gmail.com>
2021-10-16 17:12:03 +0300

    + function random number

--- trainFunc.js
+++ trainFunc.js
@@ -16,3 +16,12 @@ function sum(arr, n) {
 
   // Only change code above this line
 }
+
+function randomFraction() {
+
+  // Only change code below this line
+
+  return Math.random();
+
+  // Only change code above this line
+}
