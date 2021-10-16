90e015545cbc724caf8adb94140efd7a8941ed00
Irina Novakovich <irinaredhood@gmail.com>
2021-10-16 17:07:46 +0300

    recursive function

--- trainFunc.js
+++ trainFunc.js
@@ -8,3 +8,11 @@ do {
   i++;
 }
 while (i < 10)
+
+
+function sum(arr, n) {
+  // Only change code below this line
+  return n <= 0 ? 0 : sum(arr, n - 1) + arr[n - 1];
+
+  // Only change code above this line
+}
